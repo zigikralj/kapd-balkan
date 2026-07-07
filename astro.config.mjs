98f82f9d9471247dddf662eefe76bd4082f25b0d
Nemanja Stanojevic <nemanja.stanojevic.kv@gmail.com>
2026-07-07 18:11:41 +0200

    Localization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zigikralj.github.io', // Change this to your custom domain or https://<username>.github.io
-  // Set base dynamically: if built on GitHub Actions and not using custom domain, default to '/website'
-  base: process.env.GITHUB_ACTIONS && !process.env.CUSTOM_DOMAIN ? '/kapd-balkan/' : '/',
+  site: 'https://kapd-balkan.rs',
+  base: '/',
   output: 'static',
+  i18n: {
+    defaultLocale: 'sr',
+    locales: ['sr', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
---
+++ src/utils/translations.js
@@ -0,0 +1,124 @@
+export const translations = {
+  sr: {
+    title: "Краљевско академско природњачко друштво Балкан",
+    description: "Краљевско академско природњачко друштво Балкан (КАПД Балкан) - научно и стручно друштво основано у Краљеву.",
+    keywords: "КАПД Балкан, Краљевско академско природњачко друштво, Краљево, Натура 2000, биологија, екологија, заштита природе, Србија",
+    nav: {
+      about: "О нама",
+      natura: "Натура 2000",
+      collaborators: "Сарадници",
+      contact: "Контакт"
+    },
+    hero: {
+      explore: "Истражи",
+      scrollAria: "Скролујте до садржаја"
+    },
+    about: {
+      title: "Краљевско академско природњачко друштво Балкан",
+      text: "Краљевско академско природњачко друштво Балкан основано је 23.12.2010. године. Од свог оснивања чланови Друштва организују едукације ученика основних школа из Србије у сарадњи са Народном библиотеком \"Стефан Првовенчани\" у Краљеву, Дечијим одмаралиштем на Гочу, ПХШ \"Др. Ђорђе Радић\", Шумарском школом у Краљеву и осталим основним и средњим школама са територије града Краљева. Укупно до сад је реализовано преко 40 радионица кроз које је прошло више од 2000 учесника, као и више од 20 теренских излета у заштићена подручја нашег града и шире.",
+      images: [
+        { id: "biblioteka_1", alt: "Едукативна радионица у библиотеци" },
+        { id: "deca_2", alt: "Активности са децом на Гочу" },
+        { id: "deca_3", alt: "Теренски рад у заштићеном подручју" }
+      ]
+    },
+    natura: {
+      title: "\"Натура 2000 ресурсни центар Србије\"",
+      text: "Друштво је члан националне мреже \"Натура 2000 ресурсни центар Србије\", и активно учествује у имплементацији концепта еколошке мреже НАТУРА 2000 у сарадњи са државним органима и институцијама. Организовало је научно-стручно предавање на тему порекла и настанка заразних вирусних болести у сарадњи са библиотеком у Краљеву као и бројна друга предавања и промоције књига из области биологије, екологије и заштите животне средине (\"Белоглави суп\", \"Приручник о дневним лептирима\", књига \"Основи екологије\"). Научни и стручни сарадник Друштва је Ветеринарски специјалистички институт Краљево.",
+      imageAlt: "Натура 2000 Србија центар"
+    },
+    collaborators: {
+      title: "Сарадници",
+      text: "Друштво је било носилац великог броја пројеката финансираних од стране Министарства просвете и науке, односно Центра за промоцију науке, града Краљева, ЈП \"Србијашуме\", НП \"Ђердап\", СРП \"Пештерско поље\", и других. Сарадници на пројектима су Еколошко истраживачко друштво \"Младен Караман\" из Крагујевца, Институт за биологију и екологију ПМФ Крагујевац, Ветеринарски специјалистички институт Краљево, Српско биолошко друштво \"Стеван Јаковљевић\" и други. Чланови Друштва раде на програму истраживања биодиверзитета планина Гоч, Чемерно, Голија, Столови, Жељин, НП Ђердап, Пештер, Лазарев кањон, и осталим.",
+      partners: {
+        srbijasume: "ЈП Србијашуме",
+        VSI_KV: "Ветеринарски специјалистички институт Краљево",
+        NP_Djerdap: "Национални парк \"Ђердап\"",
+        biblioteka_kv: "Народна библиотека \"Стефан Првовенчани\" Краљево",
+        sumarska_kv: "Шумарска школа Краљево",
+        poljoprivredna_kv: "ПХШ \"Др. Ђорђе Радић\" Краљево",
+        pmf_kg: "Институт за биологију и екологију ПМФ Крагујевац",
+        sbd_stevan_jakovljevic: "СБД \"Стеван Јаковљевић\"",
+        eid_mladen_karaman: "Еколошко истраживачко друштво \"Младен Караман\""
+      }
+    },
+    social: {
+      title: "Балкан на друштвеним мрежама"
+    },
+    contact: {
+      phone: "Телефон:",
+      email: "Електронска пошта:",
+      addressLabel: "Адреса:",
+      addressValue: "Жичка 34, 36000 Краљево",
+      copyright: "КАПД Балкан. Сва права задржана. Израда сајта:",
+      floatingPhone: "Позовите нас"
+    },
+    lightbox: {
+      close: "Затвори галерију",
+      prev: "Претходна слика",
+      next: "Следећа слика",
+      ariaLabel: "Галерија слика"
+    }
+  },
+  en: {
+    title: "Kraljevsko akademsko prirodnjačko društvo Balkan",
+    description: "Kraljevsko akademsko prirodnjačko društvo Balkan (KAPD Balkan) - scientific and professional society founded in Kraljevo.",
+    keywords: "KAPD Balkan, Kraljevsko akademsko prirodnjačko društvo, Kraljevo, Natura 2000, biology, ecology, nature protection, Serbia",
+    nav: {
+      about: "About Us",
+      natura: "Natura 2000",
+      collaborators: "Collaborators",
+      contact: "Contact"
+    },
+    hero: {
+      explore: "Explore",
+      scrollAria: "Scroll to content"
+    },
+    about: {
+      title: "Kraljevsko akademsko prirodnjačko društvo Balkan",
+      text: "Kraljevsko akademsko prirodnjačko društvo Balkan was founded on December 23, 2010. Since its inception, members of the Society have organized educational programs for elementary school students in Serbia in cooperation with the \"Stefan Prvovencani\" National Library in Kraljevo, the Children's Resort on Goč, PHŠ \"Dr. Djordje Radic\", the Forestry School in Kraljevo, and other primary and secondary schools in the territory of the city of Kraljevo. To date, over 40 workshops have been realized with more than 2,000 participants, as well as more than 20 field trips to protected areas of our city and beyond.",
+      images: [
+        { id: "biblioteka_1", alt: "Educational workshop in the library" },
+        { id: "deca_2", alt: "Activities with children on Goč" },
+        { id: "deca_3", alt: "Field work in a protected area" }
+      ]
+    },
+    natura: {
+      title: "\"Natura 2000 Resource Center Serbia\"",
+      text: "The Society is a member of the national network \"Natura 2000 Resource Center Serbia\", and actively participates in the implementation of the NATURA 2000 ecological network concept in cooperation with state authorities and institutions. It has organized scientific and professional lectures on the origin and emergence of infectious viral diseases in cooperation with the library in Kraljevo, as well as numerous other lectures and book promotions in the fields of biology, ecology, and environmental protection (\"Griffon Vulture\", \"Handbook on Butterflies\", \"Fundamentals of Ecology\"). The scientific and professional associate of the Society is the Veterinary Specialist Institute Kraljevo.",
+      imageAlt: "Natura 2000 Serbia Center"
+    },
+    collaborators: {
+      title: "Collaborators",
+      text: "The Society has been the lead organization for a large number of projects funded by the Ministry of Education and Science, namely the Center for the Promotion of Science, the City of Kraljevo, JP \"Srbijasume\", NP \"Djerdap\", SRP \"Pestersko polje\", and others. Project collaborators include the \"Mladen Karaman\" Ecological Research Society from Kragujevac, the Institute of Biology and Ecology PMF Kragujevac, the Veterinary Specialist Institute Kraljevo, the \"Stevan Jakovljevic\" Serbian Biological Society, and others. Members of the Society work on the biodiversity research program of mountains Goč, Čemerno, Golija, Stolovi, Željin, NP Djerdap, Pešter, Lazarev Canyon, and others.",
+      partners: {
+        srbijasume: "JP Srbijasume",
+        VSI_KV: "Veterinary Specialist Institute Kraljevo",
+        NP_Djerdap: "National Park \"Djerdap\"",
+        biblioteka_kv: "\"Stefan Prvovencani\" National Library Kraljevo",
+        sumarska_kv: "Forestry School Kraljevo",
+        poljoprivredna_kv: "PHŠ \"Dr. Djordje Radic\" Kraljevo",
+        pmf_kg: "Institute of Biology and Ecology PMF Kragujevac",
+        sbd_stevan_jakovljevic: "SBD \"Stevan Jakovljevic\"",
+        eid_mladen_karaman: "Ecological Research Society \"Mladen Karaman\""
+      }
+    },
+    social: {
+      title: "Balkan on Social Media"
+    },
+    contact: {
+      phone: "Phone:",
+      email: "Email:",
+      addressLabel: "Address:",
+      addressValue: "Zicka 34, 36000 Kraljevo",
+      copyright: "KAPD Balkan. All rights reserved. Website development:",
+      floatingPhone: "Call us"
+    },
+    lightbox: {
+      close: "Close gallery",
+      prev: "Previous image",
+      next: "Next image",
+      ariaLabel: "Image gallery"
+    }
+  }
+};
